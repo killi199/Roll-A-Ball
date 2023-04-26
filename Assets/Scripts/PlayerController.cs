10f795e771c203e02343754a032f41f5c633cfd8
killi199 <killi199@gmx.de>
2023-04-26 19:26:04 +0200

    Moving the Player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody _rigidbody;
+    private float _movementX;
+    private float _movementY;
+    
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        var movementVector = movementValue.Get<Vector2>();
+
+        _movementX = movementVector.x;
+        _movementY = movementVector.y;
+    }
+
+    private void FixedUpdate()
+    {
+        var movement = new Vector3(_movementX, 0.0f, _movementY);
+        
+        _rigidbody.AddForce(movement * speed);
+    }
+}
